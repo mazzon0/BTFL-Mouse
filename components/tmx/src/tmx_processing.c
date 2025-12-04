e8633a761517302d7f7003eb3dff7bbffd4a1437
Federico-Rubino <fdg.rubino@gmail.com>
2025-12-04 09:50:45 +0100

    Data matrices + Circular buffer
    [tmx_processing] Add matrices for signal processing and blob detection
    [tmx_utils] Create tmx_utils with void* circular buffer type definition and methods for write and reading the circular buffer

--- components/tmx/src/tmx_processing.c
+++ components/tmx/src/tmx_processing.c
@@ -1,3 +1,7 @@
 #include "tmx_processing.h"
 
-static uint32_t raw_data[TMX_M][TMX_N];
+static uint32_t s_raw_data[TMX_M][TMX_N]; //Raw touch data
+static uint32_t s_filtered_data[TMX_M][TMX_N]; //first filetring using EMWA, fast variation
+static uint32_t s_adaptive_baseline[TMX_M][TMX_N];//low variation for adaptive baseline
+static uint32_t s_delta_signal[TMX_M][TMX_N]; //delta signal = filtered - baseline
+static tmx_touch_t s_current_frame[MAX_NUM_TOUCHES]; //curfrent detected blobs
--- components/tmx/src/tmx_processing.h
+++ components/tmx/src/tmx_processing.h
@@ -4,4 +4,15 @@
 #include "tmx_driver.h"
 #include "tmx_common.h"
 
+#define MAX_NUM_TOUCHES 2
+#define MAX_NUM_FRAMES 20
+
+typedef struct {
+    int ID;
+    uint16_t centroid_x;
+    uint16_t centroid_y;
+    uint32_t delta_peak;
+    uint32_t area;
+} tmx_touch_t;
+
 #endif // TMX_PROCESSING_H
